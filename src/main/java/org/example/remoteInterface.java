7387abda31fe0b4a04707b809c00f6245b0ce527
Luis David <david12.29@hotmail.com>
2024-02-23 11:44:11 -0500

    Update, remoteInterface is working

--- src/main/java/org/example/remoteInterface.java
+++ src/main/java/org/example/remoteInterface.java
@@ -1,8 +1,9 @@
 package org.example;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface remoteInterface {
+public interface remoteInterface extends Remote {
     int[][] creatSudoku(int tamaño) throws RemoteException;
     int[][] fillMatrix(int tamaño, int[][] sudoku) throws RemoteException;
     void showMatrix(int[][] sudoku) throws RemoteException;
