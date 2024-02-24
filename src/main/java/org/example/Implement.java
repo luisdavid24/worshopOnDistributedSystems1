e9842e83f5ceb6aab084f0ea5dbbbd6722d2decf
Luis David <david12.29@hotmail.com>
2024-02-24 18:27:46 -0500

    Update,creatSudoku is finished

--- src/main/java/org/example/Implement.java
+++ src/main/java/org/example/Implement.java
@@ -11,13 +11,25 @@ public class Implement extends UnicastRemoteObject implements remoteInterface{
 
 
     @Override
-    public int[][] creatSudoku(int tamaño) throws RemoteException {
+    public int[][] creatSudoku(int n) throws RemoteException {
+        int[][] matrix=new int[n][];
+        for (int i=0;i<n;i++){
+            matrix[i]=new int[n];
+        }
+        for (int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println("  ");
+        }
         System.out.println("Estamos usando creatSudoku");
         return new int[0][];
     }
-
+    /*
+    * [ [1,2,3,4],[2,3,4,5],[],[] ]
+    * */
     @Override
-    public int[][] fillMatrix(int tamaño, int[][] sudoku) throws RemoteException {
+    public int[][] fillMatrix(int n, int[][] sudoku) throws RemoteException {
         System.out.println("Estamos usando fillMatrix");
         return new int[0][];
     }
