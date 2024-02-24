674366ec382f5012c61f755f12e55829a21cc621
Luis David <david12.29@hotmail.com>
2024-02-24 18:28:11 -0500

    Update,creatSudoku is finished 2

--- src/main/java/org/example/Implement.java
+++ src/main/java/org/example/Implement.java
@@ -16,14 +16,8 @@ public class Implement extends UnicastRemoteObject implements remoteInterface{
         for (int i=0;i<n;i++){
             matrix[i]=new int[n];
         }
-        for (int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(matrix[i][j]+" ");
-            }
-            System.out.println("  ");
-        }
-        System.out.println("Estamos usando creatSudoku");
-        return new int[0][];
+
+        return matrix;
     }
     /*
     * [ [1,2,3,4],[2,3,4,5],[],[] ]
